baaee69045d9777c14628a1e214c29847225739f
parastoopourazizi <napourazizi@gmail.com>
2023-12-13 00:14:46 +0330

    edit navbar

--- src/components/navbar/MyNavbar.js
+++ src/components/navbar/MyNavbar.js
@@ -1,31 +1,30 @@
 
 import Container from 'react-bootstrap/Container';
-
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 import Offcanvas from 'react-bootstrap/Offcanvas';
 function MyNavbar(){
     const expand = "md";
     return(
-        <Navbar key={expand} style={{backgrandColor : '#ccc'}} expand={expand}  className=" mb-3">
+        <Navbar key={expand} style={{backgrandColor : '#bbb'}} expand={expand}  className=" mb-3">
           <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+            <Navbar.Brand href="#"> تاچ لرن</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+              placement="start"
             >
               <Offcanvas.Header closeButton>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
+                  تاچ لرن
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <Nav.Link href="#action1">صفحه اصلی</Nav.Link>
+                  <Nav.Link href="#action2">ساخت مقاله</Nav.Link>
+                  <Nav.Link href="#action2"> درباره ما</Nav.Link>
                   
                 </Nav>
                 
