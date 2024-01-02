31c2c28aa1625504d4926f95794860c8c2b95219
parastoopourazizi <napourazizi@gmail.com>
2024-01-02 14:56:44 +0330

    add post

--- src/pages/addArticel/AddArticle.js
+++ src/pages/addArticel/AddArticle.js
@@ -7,21 +7,22 @@ import { useState } from "react";
 
 
 function AddArticle(){
-  const [titleState , setTitleState] = useState('')
-    const AddArticleHandler = () =>{
-      axios.post('http://localhost:5000/article',{
-        title:'عنوان مقاله دهم',
-        desc:'متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم ',
-        writter:'میلاد',
-        category:'وب',
-        image:"https://dl.next1code.ir/images/react/article6.webp",
-        readingtime: '5'       
-      })
-    }
-    const titleInputHandler = (e) =>{
-      setTitleState(e.target.value)
+  
+  const [formData , setFormData]= useState({})
 
-    }
+  const addArticleHandler = ()=>{
+    axios.post('http://localhost:5000/article' , formData)
+
+  }
+  
+  const formHandler = ( e , propertyName)=> {
+    setFormData( {...formData , [propertyName] : e.target.value})
+    
+  }
+       
+    
+    
+  
     return(
        <>
        <MyNavbar/>
@@ -29,36 +30,36 @@ function AddArticle(){
        <Form>
       <Form.Group className="mb-3">
         <Form.Label> عنوان مقاله</Form.Label>
-        <Form.Control onChange={titleInputHandler} type="text" placeholder="عنوان مقاله را وارد کنید" />
+        <Form.Control onChange={(e) => formHandler(e , 'title')} type="text" placeholder="عنوان مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>  توضیح کوتاه</Form.Label>
-        <Form.Control type="text" placeholder="یک توضیح کوتاه در مقاله را وارد کنید" />
+        <Form.Control onChange={(e) => formHandler(e , 'desc')} type="text" placeholder="یک توضیح کوتاه در مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   نویسنده مقاله</Form.Label>
-        <Form.Control type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
+        <Form.Control onChange={(e) => formHandler(e , 'wrriter')} type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   موضوع مقاله</Form.Label>
-        <Form.Control type="text" placeholder="موضوع مقاله را وارد کنید" />
+        <Form.Control onChange={(e) => formHandler(e , 'category')} type="text" placeholder="موضوع مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   عکس مقاله</Form.Label>
-        <Form.Control type="text" placeholder=" آدرس عکس مقاله را وارد کنید" />
+        <Form.Control onChange={(e) => formHandler(e , 'image')} type="text" placeholder=" آدرس عکس مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   مدت زمان خواندن مقاله</Form.Label>
-        <Form.Control type="number" placeholder="" />
+        <Form.Control onChange={(e) => formHandler(e , 'readingTime')} type="number" placeholder="" />
       </Form.Group>
 
      
-      <Button onClick={AddArticleHandler} variant="primary" type="button">
+      <Button onClick={addArticleHandler} variant="primary" type="button">
         ساخت مقاله
       </Button>
     </Form>
