cef840f5d328ffe7723885f24c7daf2fa18110c5
parastoopourazizi <napourazizi@gmail.com>
2024-01-04 12:55:43 +0330

    optimal

--- src/pages/addArticel/AddArticle.js
+++ src/pages/addArticel/AddArticle.js
@@ -30,32 +30,44 @@ function AddArticle(){
        <Form>
       <Form.Group className="mb-3">
         <Form.Label> عنوان مقاله</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'title')} type="text" placeholder="عنوان مقاله را وارد کنید" />
+        <Form.Control
+        name="title"
+         onChange={ formHandler} type="text" placeholder="عنوان مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>  توضیح کوتاه</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'desc')} type="text" placeholder="یک توضیح کوتاه در مقاله را وارد کنید" />
+        <Form.Control
+        name="desc"
+         onChange={ formHandler} type="text" placeholder="یک توضیح کوتاه در مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   نویسنده مقاله</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'wrriter')} type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
+        <Form.Control
+        name="writer"
+         onChange={ formHandler} type="text" placeholder="نام نویسنده مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   موضوع مقاله</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'category')} type="text" placeholder="موضوع مقاله را وارد کنید" />
+        <Form.Control
+        name="category"
+        onChange={ formHandler} type="text" placeholder="موضوع مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   عکس مقاله</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'image')} type="text" placeholder=" آدرس عکس مقاله را وارد کنید" />
+        <Form.Control
+        name="image"
+        onChange={ formHandler} type="text" placeholder=" آدرس عکس مقاله را وارد کنید" />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>   مدت زمان خواندن مقاله</Form.Label>
-        <Form.Control onChange={(e) => formHandler(e , 'readingTime')} type="number" placeholder="" />
+        <Form.Control
+        name="readingTime"
+         onChange={ formHandler} type="number" placeholder="" />
       </Form.Group>
 
      
