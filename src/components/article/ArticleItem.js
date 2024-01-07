734ad19d8efd3cf2e785fad48b30944d8b871f36
parastoopourazizi <napourazizi@gmail.com>
2024-01-07 14:03:01 +0330

    add link in article

--- src/components/article/ArticleItem.js
+++ src/components/article/ArticleItem.js
@@ -1,6 +1,7 @@
 import { TiArrowLeftThick } from "react-icons/ti";
 import Card from 'react-bootstrap/Card';
 import { BiTime } from "react-icons/bi";
+import { Link } from "react-router-dom";
 import './ArticleItem.css'
 
 function ArticleItem(props){
@@ -12,11 +13,14 @@ function ArticleItem(props){
           <Card.Text>
           {props.desc}
           </Card.Text>
+         <Link to={`/article/${props.id}`}>
          <span className="read-more">
             <span>ادامه مقاله</span>
             <TiArrowLeftThick size='25' />
 
          </span>
+
+         </Link>
         </Card.Body>
        <Card.Footer className='d-flex justify-content-between align-items-center py-3'>
         <span>نویسنده:{props.writter}</span>
